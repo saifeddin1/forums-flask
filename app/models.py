1859fca486fa8101bf569d97bcd62f0bdc1d23a9
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-03-05 11:06:05 +0100

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,26 @@
+#--------------------------- Models -------------------------------
+
+import datetime
+
+
+class Member:
+    def __init__(self, name, age):
+        self.id = 0
+        self.name = name
+        self.age = age
+        self.posts = []
+
+    def __str__(self):
+        return "Name: {}, Age: {}".format(self.name, self.age)
+
+
+class Post:
+    def __init__(self, title, content, member_id=0):
+        self.id = 0
+        self.title = title
+        self.content = content
+        self.member_id = member_id
+        self.date = datetime.datetime.now()
+
+    def __str__(self):
+        return f"Title: {self.title}, Content: {self.content}, Date: {self.date}"
