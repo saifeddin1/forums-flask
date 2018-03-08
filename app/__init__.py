3718495b85604697aa1fd859fd883449badb5356
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-03-09 00:23:43 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,11 @@
-from flask import  Flask
-import stores
-import dummy_data
+from flask import Flask
+from app import stores, models, dummy_data
 
 app = Flask(__name__)
 
 member_store = stores.MemberStore()
 post_store = stores.PostStore()
+dummy_data.seed_stores(member_store, post_store)
 
-from views import *
+from app import views
 
-if __name__ == '__main__':
-    dummy_data.seed_stores(member_store, post_store)
-    
