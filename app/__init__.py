22d1791346dd9a3cd9f281cb5515667358652322
Saifeddin Matoui <saifeddinmatoui4@gmail.com>
2018-03-15 21:42:32 +0100

    add whitespace

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 member_store = stores.MemberStore()
 post_store = stores.PostStore()
 dummy_data.seed_stores(member_store, post_store)
+
 from app import api
 from app import views
 
